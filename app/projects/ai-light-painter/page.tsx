0c837c0a14b61dd8cd33e6e6fc9dc41a3e613fa2
Sam Vance <samvance243@gmail.com>
2026-01-22 12:54:15 -0500

    updated blurbs for ai painter. haven't added all photos/vids

--- app/projects/ai-light-painter/page.tsx
+++ app/projects/ai-light-painter/page.tsx
@@ -42,17 +42,17 @@ const galleryTemplateProps: GalleryTemplateProps = {
           type: "video",
           src: "https://cdn.briannavance.com/sam-vance-website/lego-light-painting1.mp4",
         },
-        title: "Initial Concept",
+        title: "Lego Prototype",
         description: `This is a short video of the lego prototype based on the previous sketch.
 
           After building this prototype, I realized that the arm design was too unstable and wobbly to create high quality light paintings. Furthermore, the design made it difficult to draw shapes that require precise horizontal movements as the arm moved in an arc rather than a straight line. While doable, I decided to go back to the drawing board and come up with a more stable design that could also achieve straight line movements.`,
       },
-      {
-        media: { type: "video", src: "/projects/humanoid-legs/IMG_0640.mp4" },
-        title: "Initial Concept",
-        description:
-          "Here is a small description of the results that were achieved for this project.",
-      },
+      // {
+      //   media: { type: "video", src: "/projects/humanoid-legs/IMG_0640.mp4" },
+      //   title: "Initial Concept",
+      //   description:
+      //     "Here is a small description of the results that were achieved for this project.",
+      // },
     ],
   },
   partTwoProps: {
@@ -101,8 +101,11 @@ const galleryTemplateProps: GalleryTemplateProps = {
           src: "/projects/humanoid-legs/concept.mp4",
         },
         title: "Initial Results",
-        description:
-          "Here is a small description of how the initial concept was developed.",
+        description: `I experimented with different LED brightnesses to determine what would create the best light painting effects. I took the RGB LED and waved it around in front of a camera with a long exposure setting to capture the light trails.
+          
+          As you can see in the photos, the initial results were too washed out, resulting in white lines rather than distinct colors. After adjusting the LED brightness I was able to achieve distinct colors in the light paintings.
+          
+          Once I had the brightness dialed in, I had the robot create a few test light paintings. These were made from me scribbling out a quick circle, so they aren't impressive, but they showed that the robot was capable of creating light paintings.`,
       },
       {
         media: {
@@ -110,9 +113,14 @@ const galleryTemplateProps: GalleryTemplateProps = {
           src: "/projects/humanoid-legs/process.jpg",
           alt: "Process",
         },
-        title: "Results",
-        description:
-          "Here is a small description of the process that was conducted for this project.",
+        title: "Light Paintings",
+        description: `After I had the robot working, I created a few light paintings with it. Here are some photos of the first few light paintings it created.
+          
+          As you'll see in the first rocketship photo, there was an issue with the floss slipping (or potentially stretching) slightly on the motor wheel. This caused the horizontal positioning to be slightly off which is why the rocketship fire seems to be coming out of the side rather than straight out the bottom.
+          
+          I was able to fix this issue in later light paintings by commanding the robot to draw parts close together all, at once instead of jumping back and forth between multiple sections of the drawing. This way, even if the floss slipped a little, the overall drawing would still look correct.
+          
+          This issue could obviously be solved by higher quality parts and some type of calibration routine, but it worked well enough that I was happy with the results.`,
       },
       {
         media: {
@@ -120,9 +128,14 @@ const galleryTemplateProps: GalleryTemplateProps = {
           src: "/projects/humanoid-legs/results.jpg",
           alt: "Results",
         },
-        title: "Results",
-        description:
-          "Here is a small description of the results that were achieved for this project.",
+        title: "Stop Motion Videos",
+        description: `I am by no means an artist, but I wanted to create something a little more in line with what had inspired me to create this robot in the first place--stop motion light painting videos.
+          
+          To do this, I created a series of light paintings with slight variations between each one. Then, I compiled the photos into a stop motion video.
+          
+          These videos are far from perfect, but I was able to create a few that I was happy with. The baseball one is my favorite since it shows off the precision that dental floss can provide as it matched my hand-writing for the text quite well.
+          
+          Each of these stop motions took around an hour to create. Mostly this was due to my lack of artistic abilities. The robot would take roughly 1 minute to create each light painting. Since my iPhone could only do a maximum of 30 second exposures, I would pause the robot halfway through the drawing to let me start the next photo.`,
       },
     ],
   },
